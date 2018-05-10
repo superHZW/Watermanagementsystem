f95926e82670b172c85bc7ab919f361a2565a3ca
superHZW <1528247721@qq.com>
2018-05-10 23:20:39 +0800

    新增进水池匹配demo

--- html/js/models/joint.shapes.app.js
+++ html/js/models/joint.shapes.app.js
@@ -6,79 +6,10 @@
   joint.shapes.app.Connector = joint.shapes.basic.Circle.extend({
     defaults: _.defaultsDeep({
       type: "app.Connector",
-      modelType:'node',
+      modelType: 'node',
     }, joint.shapes.basic.Circle.prototype.defaults)
   });
-  
-  joint.shapes.app.pipe = joint.shapes.basic.Generic.extend({
-    markup: [
-      '<g class="rotatable">',
-      '<g class="scalable">',
-      '<polyline points="0,0 0,80 0,60 60,60 60,80 60,0 60,20 0,20" style="fill: DodgerBlue;stroke:#000000;stroke-width:1"/>',
-      '</g>',
-      '</g>'
-    ].join(''),
-    portMarkup: '<circle class="port-body"/>',
 
-    defaults: joint.util.deepSupplement({
-      type: 'app.pipe',
-      modelType: 'pipe',
-      modelText: '管道',
-      attrs: {
-        '.': {
-          magnet: !1,
-          'data-tooltip': '管道',
-          'data-tooltip-position': 'left',
-          'data-tooltip-position-selector': '.joint-stencil'
-        },
-        text: {
-          text: '管道',
-        },
-      },
-      ports: {
-        groups: {
-          'out': {
-            position: {
-              name: 'absolute',
-              args: {
-                x: '100%',
-                y: '50%'
-              }
-            },
-            attrs: {
-              circle: {
-                fill: '#008B00',
-                stroke: '#008B00',
-                'stroke-width': 2,
-                r: 3,
-                magnet: true
-              }
-            }
-          },
-
-          'in': {
-            position: {
-              name: 'absolute',
-              args: {
-                x: '0%',
-                y: '50%'
-              }
-            },
-            attrs: {
-              circle: {
-                fill: '#FF0000',
-                stroke: '#FF0000',
-                'stroke-width': 2,
-                r: 3,
-                magnet: true
-              }
-            }
-          },
-        },
-      },
-
-    }, joint.shapes.basic.Generic.prototype.defaults)
-  });
 
   joint.shapes.devs.Mypipe = joint.shapes.devs.Model.extend({
     markup: [
@@ -89,7 +20,6 @@
       '</g>',
     ].join(''),
     defaults: joint.util.deepSupplement({
-
       type: 'devs.Mypipe',
       size: {
         width: 60,
@@ -101,8 +31,7 @@
           'data-tooltip-position': 'left',
           'data-tooltip-position-selector': '.joint-stencil'
         },
-      }
-
+      },
     }, joint.shapes.devs.Model.prototype.defaults)
   });
 
@@ -117,10 +46,12 @@
 
     defaults: joint.util.deepSupplement({
       type: 'devs.MyInsump',
+      modeltext: "进水池",
       size: {
         width: 70,
         height: 100
       },
+      name: '进水池',
       attrs: {
         '.': {
           'data-tooltip': '进水池',
@@ -130,7 +61,7 @@
         image: {
           'xlink:href': 'assets/svg/进水池.svg',
         },
-      }
+      },
     }, joint.shapes.devs.Model.prototype.defaults)
   });
 
@@ -253,5 +184,6 @@
 
     }, joint.shapes.devs.Model.prototype.defaults)
   });
+
 
 })(joint);
